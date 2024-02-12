efd2cf4cfad0c122f4a09cf12911cb2e7af5079d
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-12 20:38:27 +0700

    Get current os system

---
+++ main.py
@@ -0,0 +1,19 @@
+# Side projects to delete certain filetypes in a path and also check whether the file is openning, if yes
+# close it and delete it.
+
+import os
+
+# Get current operating system
+os_name = os.name
+
+# Append the filetypes you want to delete
+delete_filetypes = ["xls"]
+
+# The path you want to delete
+
+
+def main(current_os):
+    print(current_os)
+
+
+main(os_name)
